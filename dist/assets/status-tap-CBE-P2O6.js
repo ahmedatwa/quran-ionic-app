6c523956e848a0c187e4621173bfbe5a32d44555
Ahmed Atwa <aatwa86@gmail.com>
2024-08-05 17:15:55 +0100

    update

--- dist/assets/status-tap-CBE-P2O6.js
+++
@@ -1,3 +0,0 @@
-import{aL as r,aM as i,aN as c,aO as d,aP as l}from"./index-BxKZXudA.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
