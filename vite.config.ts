73048f2a10417e6e6ab023491d3c4419d203aa59
Ahmed Atwa <aatwa86@gmail.com>
2025-04-19 19:36:22 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /// <reference types="vitest" />
 
-import legacy from "@vitejs/plugin-legacy";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), legacy()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
